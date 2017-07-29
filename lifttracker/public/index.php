1610cd44e3bd9c34ca36e7b6a584a591ee5d673b
Wesley Garey <wesley@ls1.localdomain>
2017-07-28 22:22:01 -0400

    Got rid of public and private folders

--- lifttracker/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('../private/definitions/paths-class.php');
-    $paths = new Paths();    
-    require_once($paths->getControllerClassFile());
-    
-    $controller = new Controller($paths);
-    $controller->run();
-?>
