7365d4e8c382d10e84765ea92ef5999cd0d067af
Wes G <w.d.garey@eagle.clarion.edu>
2018-02-18 22:27:03 -0500

    Changed message appearance

--- lifttracker/includes/message.php
+++ lifttracker/includes/message.php
@@ -1,13 +1,14 @@
-<?php
-if (isset($msg) && strlen($msg)) {
-  echo(htmlspecialchars($msg));
-}
-if (isset($msgList)) {
-?>
-<ul>
-  <?php foreach ($msgList as $msgItem) { ?>
-    <li><?php echo(htmlspecialchars($msgItem)); ?></li>
-  <?php } ?>
-</ul>
+<?php if (isset($msg) && strlen($msg)) { ?>
+  <div class="center-block text-center text-danger">
+    <h2>
+      <?php echo(htmlspecialchars($msg)); ?>
+    </h2>
+    <?php if (isset($msgList)) { ?>
+      <ul>
+        <?php foreach ($msgList as $msgItem) { ?>
+          <li><h3><?php echo(htmlspecialchars($msgItem)); ?></h3></li>
+        <?php } ?>
+      </ul>
+    <?php } ?>
+  </div>
 <?php } ?>
-
