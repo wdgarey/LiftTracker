020dab58776ede8e898871e8725fab4b53ec024f
Wesley Garey <whitewez@hotmail.com>
2019-12-20 09:46:52 -0500

    Made weeks and days collapsable in plan view

--- lifttracker/view/plan-view.php
+++ lifttracker/view/plan-view.php
@@ -21,14 +21,14 @@
           <div class="panel panel-default">
   <?php foreach ($weeks as $week) { ?>
             <div class="panel-heading">
-              <h3><?php echo(htmlspecialchars($week->getTitle())); ?></h3>
+              <h3><a href="#week<?php echo(htmlspecialchars($week->getId())); ?>" data-toggle="collapse"><?php echo(htmlspecialchars($week->getTitle())); ?></a></h3>
               <a href="../main/index.php?controller=week&action=weekedit&weekid=<?php echo(htmlspecialchars($week->getId())); ?>">Edit</a>
               | <a data-toggle="confirmation" href="../main/index.php?controller=week&action=weekdelete&weekid=<?php echo(htmlspecialchars($week->getId())); ?>">Delete</a>
     <?php if (count($week->getDays()) > 0) { ?>
               | <a href="../main/index.php?controller=day&action=dayadd&weekid=<?php echo(htmlspecialchars($week->getId())); ?>">Add Day</a>
     <?php }?>
             </div>
-            <div class="panel-body">
+            <div id="week<?php echo(htmlspecialchars($week->getId())); ?>" class="panel-body collapse">
     <?php if (count($week->getDays()) == 0) { ?>
             <h3>No days yet? Add one <a href="../main/index.php?controller=day&action=dayadd&weekid=<?php echo(htmlspecialchars($week->getId())); ?>">here</a>.</h3>
     <?php } else {?>
@@ -37,14 +37,14 @@
                 <div class="col-md-4">
                   <div class="panel panel-default">
                     <div class="panel-heading">
-                      <h5><?php echo(htmlspecialchars($day->getTitle())); ?></h5>
+                      <h5><a href="#day<?php echo(htmlspecialchars($day->getId())); ?>" data-toggle="collapse"><?php echo(htmlspecialchars($day->getTitle())); ?></a></h5>
                       <a href="../main/index.php?controller=day&action=dayedit&dayid=<?php echo(htmlspecialchars($day->getId())); ?>">Edit</a>
                       | <a data-toggle="confirmation" href="../main/index.php?controller=day&action=daydelete&dayid=<?php echo(htmlspecialchars($day->getId())); ?>">Delete</a>
         <?php if (count($day->getExercises()) > 0) { ?>
                       | <a href="../main/index.php?controller=exercise&action=exerciseadd&dayid=<?php echo(htmlspecialchars($day->getId())); ?>">Add Exercise</a>
         <?php } ?>
                     </div>
-                    <div class="panel-body">
+                    <div id="day<?php echo(htmlspecialchars($day->getId())); ?>" class="panel-body collapse">
         <?php if (count($day->getExercises()) == 0) { ?>
                       No Exercises yet? Add one <a href="../main/index.php?controller=exercise&action=exerciseadd&dayid=<?php echo(htmlspecialchars($day->getId())); ?>">here</a>.
           <?php } else {?>
